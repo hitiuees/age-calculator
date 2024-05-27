55e7075b67f916c01b745bff59e9798d2cc8928c
hitiuees <ghbadr132800@gmail.com>
2024-05-27 13:45:04 +0100

    final commit

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("sbmit").onclick = function() {
+        let year = document.getElementById("form").textContent.trim();
+        let yearInput = parseInt(year);
 
+        if (isNaN(yearInput) || year === "") {
+            alert("Please enter your birth year");
+        } else {
+            let currentYear = 2024;
+            let age = currentYear - yearInput;
+            window.alert(`Your age is ${age}`);
+        }
+    }
+});
 
 
 
